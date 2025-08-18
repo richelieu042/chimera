f8126a4a88aff2ee67ebf9bacf679bd39c9e6417
Richelieu <richelieu042@gmail.com>
2025-08-18 13:24:11 +0800

    update

--- src/atomic/gtypeKit/uint.go
+++ src/atomic/gtypeKit/uint.go
@@ -2,8 +2,10 @@ package gtypeKit
 
 import "github.com/gogf/gf/v2/container/gtype"
 
-var NewUint func(value ...uint) *gtype.Uint = gtype.NewUint
+var (
+	NewUint func(value ...uint) *gtype.Uint = gtype.NewUint
 
-var NewUint32 func(value ...uint32) *gtype.Uint32 = gtype.NewUint32
+	NewUint32 func(value ...uint32) *gtype.Uint32 = gtype.NewUint32
 
-var NewUint64 func(value ...uint64) *gtype.Uint64 = gtype.NewUint64
+	NewUint64 func(value ...uint64) *gtype.Uint64 = gtype.NewUint64
+)
