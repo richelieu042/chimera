8bbf8732eff6f35030084425a5b9492489114df2
Richelieu <richelieu042@gmail.com>
2026-01-08 13:58:25 +0800

    update

--- src/image/imageKit/resize_path.go
+++ src/image/imageKit/resize_path.go
@@ -1,6 +1,7 @@
 package imageKit
 
 import (
+	"image"
 	"image/jpeg"
 	"image/png"
 
@@ -9,33 +10,29 @@ import (
 	"github.com/richelieu-yang/chimera/v3/src/file/fileKit"
 )
 
-// Resize 缩放图片到指定尺寸（不保证纵横比）
-/*
-	@param srcPath	源图片文件路径
-	@param dstPath	目标图片文件路径
-	@param width	目标宽度（像素）
-	@param height	目标高度（像素）
-	@return 返回错误信息，如果成功则返回 nil
-*/
-func Resize(srcPath, dstPath string, width, height int) (err error) {
-	// (0) 参数检查
+func processSrc(srcPath string) (image.Image, string, error) {
 	if err := strKit.AssertNotBlank(srcPath, "srcPath"); err != nil {
-		return err
+		return nil, "", err
 	}
+
+	srcImg, srcFormat, err := OpenAndDecode(srcPath)
+	if err != nil {
+		return nil, "", errorKit.Wrapf(err, "fail to decode source image")
+	}
+	srcExt := "." + srcFormat
+
+	return srcImg, srcExt, nil
+}
+
+func processDst(dstPath string, dstImg image.Image, srcExt string) (err error) {
 	if err := strKit.AssertNotBlank(dstPath, "dstPath"); err != nil {
 		return err
 	}
 
-	// (1) 打开源图片文件 && 解码图片（自动识别格式）
-	srcImg, srcFormat, err := OpenAndDecode(srcPath)
-	if err != nil {
-		return errorKit.Wrapf(err, "fail to decode source image")
-	}
-
-	// (2) 确定目标文件的格式 && 创建文件（会自动创建父目录）
+	// 处理特殊情况: dstPath不带格式
 	dstExt := fileKit.GetExt(dstPath)
 	if strKit.IsEmpty(dstExt) {
-		dstExt = "." + srcFormat
+		dstExt = srcExt
 		dstPath += dstExt
 	}
 	switch dstExt {
@@ -44,6 +41,7 @@ func Resize(srcPath, dstPath string, width, height int) (err error) {
 	default:
 		return errorKit.Newf("unsupported dstExt: %s", dstExt)
 	}
+
 	dstFile, err := fileKit.Create(dstPath)
 	if err != nil {
 		return err
@@ -51,17 +49,11 @@ func Resize(srcPath, dstPath string, width, height int) (err error) {
 	defer func() {
 		dstFile.Close()
 
-		// 失败的情况下，毁尸灭迹
+		// 失败的情况下，毁尸灭迹（把生成的目标文件删了）
 		if err != nil {
 			_ = fileKit.Delete(dstPath)
 		}
 	}()
-
-	// (3) 创建目标图片对象，使用 CatmullRom 算法进行高质量缩放
-	dstImg, err := ResizeImage(srcImg, width, height)
-	if err != nil {
-		return
-	}
 
 	// (4) 根据目标文件扩展名编码保存
 	switch dstExt {
@@ -74,4 +66,54 @@ func Resize(srcPath, dstPath string, width, height int) (err error) {
 		err = errorKit.Newf("unsupported dstExt: %s", dstExt)
 	}
 	return
+}
+
+// Resize 缩放图片到指定尺寸（不保证纵横比）.
+/*
+	@param srcPath	源图片文件路径
+	@param dstPath	目标图片文件路径
+	@param width	目标宽度（像素）
+	@param height	目标高度（像素）
+	@return 返回错误信息，如果成功则返回 nil
+*/
+func Resize(srcPath, dstPath string, width, height int) (err error) {
+	srcImg, srcExt, err := processSrc(srcPath)
+	if err != nil {
+		return
+	}
+
+	dstImg, err := ResizeImage(srcImg, width, height)
+	if err != nil {
+		return
+	}
+
+	return processDst(dstPath, dstImg, srcExt)
+}
+
+func ResizeWithScale(srcPath, dstPath string, scale float64) (err error) {
+	srcImg, srcExt, err := processSrc(srcPath)
+	if err != nil {
+		return
+	}
+
+	dstImg, err := ResizeImageWithScale(srcImg, scale)
+	if err != nil {
+		return
+	}
+
+	return processDst(dstPath, dstImg, srcExt)
+}
+
+func ResizeKeepAspectRatio(srcPath, dstPath string, maxWidth, maxHeight int) (err error) {
+	srcImg, srcExt, err := processSrc(srcPath)
+	if err != nil {
+		return
+	}
+
+	dstImg, err := ResizeImageKeepAspectRatio(srcImg, maxWidth, maxHeight)
+	if err != nil {
+		return
+	}
+
+	return processDst(dstPath, dstImg, srcExt)
 }
