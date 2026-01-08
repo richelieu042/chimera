1da643079a01c220092311a4e2591902baf760bf
Richelieu <richelieu042@gmail.com>
2026-01-08 11:40:50 +0800

    update

--- internal/main.go
+++ internal/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 
-	"github.com/gogf/gf/v2/os/gfile"
+	"github.com/richelieu-yang/chimera/v3/src/image/imageKit"
 )
 
 func main() {
-	//err := gfile.RemoveFile("/Users/richelieu/Desktop/111.png")
-	err := gfile.RemoveAll("/Users/richelieu/Desktop/222.png")
+	////err := gfile.RemoveFile("/Users/richelieu/Desktop/111.png")
+	//err := gfile.RemoveAll("/Users/richelieu/Desktop/222.png")
+	//
+	//fmt.Println(err)
 
+	err := imageKit.Resize("/Users/richelieu/Desktop/iShot_2026-01-07_15.27.36.png", "222.png", 100, 100)
 	fmt.Println(err)
 }
--- src/image/imageKit/resize.go
+++ src/image/imageKit/resize.go
@@ -12,7 +12,7 @@ import (
 	"golang.org/x/image/draw"
 )
 
-// ResizeImage 缩放图片到指定尺寸
+// Resize 缩放图片到指定尺寸
 /*
 	@param srcPath	源图片文件路径
 	@param dstPath	目标图片文件路径
@@ -20,7 +20,7 @@ import (
 	@param height	目标高度（像素）
 	@return 返回错误信息，如果成功则返回 nil
 */
-func ResizeImage(srcPath, dstPath string, width, height int) (err error) {
+func Resize(srcPath, dstPath string, width, height int) (err error) {
 	// (0) 参数检查
 	if err := strKit.AssertNotBlank(srcPath, "srcPath"); err != nil {
 		return err
@@ -71,15 +71,7 @@ func ResizeImage(srcPath, dstPath string, width, height int) (err error) {
 	}()
 
 	// (3) 创建目标图片对象，使用 CatmullRom 算法进行高质量缩放
-	dstImg := image.NewRGBA(image.Rect(0, 0, width, height))
-	/*
-		支持的插值算法包括：
-			draw.NearestNeighbor - 最快但质量最低
-			draw.ApproxBiLinear - 速度和质量平衡
-			draw.BiLinear - 双线性插值
-			draw.CatmullRom - 高质量，推荐使用
-	*/
-	draw.CatmullRom.Scale(dstImg, dstImg.Bounds(), srcImg, srcImg.Bounds(), draw.Over, nil)
+	dstImg := resize(srcImg, width, height)
 
 	// (4) 根据目标文件扩展名编码保存
 	switch dstExt {
@@ -92,4 +84,18 @@ func ResizeImage(srcPath, dstPath string, width, height int) (err error) {
 		err = errorKit.Newf("unsupported dstExt: %s", dstExt)
 	}
 	return
+}
+
+func resize(src image.Image, width, height int) image.Image {
+	dst := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	/*
+		支持的插值算法包括：
+			draw.NearestNeighbor - 最快但质量最低
+			draw.ApproxBiLinear - 速度和质量平衡
+			draw.BiLinear - 双线性插值
+			draw.CatmullRom - 高质量，推荐使用
+	*/
+	draw.CatmullRom.Scale(dst, dst.Bounds(), src, src.Bounds(), draw.Over, nil)
+	return dst
 }
