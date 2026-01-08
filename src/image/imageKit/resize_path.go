ba8fba7dc3e067a65ac8c91bd839c0afb45169b0
Richelieu <richelieu042@gmail.com>
2026-01-08 16:49:02 +0800

    update

--- src/image/imageKit/resize_path.go
+++ src/image/imageKit/resize_path.go
@@ -1,46 +1,5 @@
 package imageKit
 
-import (
-	"image"
-
-	"github.com/richelieu-yang/chimera/v3/src/core/errorKit"
-	"github.com/richelieu-yang/chimera/v3/src/core/strKit"
-	"github.com/richelieu-yang/chimera/v3/src/file/fileKit"
-)
-
-func processSrc(srcPath string) (image.Image, string, error) {
-	// 检查 && 容错
-	if err := strKit.AssertNotBlank(srcPath, "srcPath"); err != nil {
-		return nil, "", err
-	}
-	srcPath = strKit.TrimSpace(srcPath)
-
-	srcImg, srcFormat, err := DecodeFromPath(srcPath)
-	if err != nil {
-		return nil, "", errorKit.Wrapf(err, "fail to decode source image")
-	}
-	srcExt := "." + srcFormat
-
-	return srcImg, srcExt, nil
-}
-
-func processDst(dstPath string, dstImg image.Image, srcExt string) (err error) {
-	// 检查 && 容错
-	if err := strKit.AssertNotBlank(dstPath, "dstPath"); err != nil {
-		return err
-	}
-	dstPath = strKit.TrimSpace(dstPath)
-
-	// 处理特殊情况: dstPath 不带格式，则使用 srcPath 的格式
-	dstExt := fileKit.GetExt(dstPath)
-	if strKit.IsEmpty(dstExt) {
-		dstExt = srcExt
-		dstPath += dstExt
-	}
-
-	return EncodeToPath(dstPath, dstImg)
-}
-
 // Resize 缩放图片到指定尺寸（不保证纵横比）.
 /*
 	@param srcPath	源图片文件路径
@@ -50,7 +9,7 @@ func processDst(dstPath string, dstImg image.Image, srcExt string) (err error) {
 	@return 返回错误信息，如果成功则返回 nil
 */
 func Resize(srcPath, dstPath string, width, height int) (err error) {
-	srcImg, srcExt, err := processSrc(srcPath)
+	srcImg, _, err := DecodeFromPath(srcPath)
 	if err != nil {
 		return
 	}
@@ -60,12 +19,12 @@ func Resize(srcPath, dstPath string, width, height int) (err error) {
 		return
 	}
 
-	return processDst(dstPath, dstImg, srcExt)
+	return EncodeToPath(dstPath, dstImg)
 }
 
 // ResizeWithScale 按指定比例缩放图片（保证纵横比）.
 func ResizeWithScale(srcPath, dstPath string, scale float64) (err error) {
-	srcImg, srcExt, err := processSrc(srcPath)
+	srcImg, _, err := DecodeFromPath(srcPath)
 	if err != nil {
 		return
 	}
@@ -75,12 +34,12 @@ func ResizeWithScale(srcPath, dstPath string, scale float64) (err error) {
 		return
 	}
 
-	return processDst(dstPath, dstImg, srcExt)
+	return EncodeToPath(dstPath, dstImg)
 }
 
 // ResizeKeepAspectRatio 按比例调整图片大小（保证纵横比；适应指定尺寸）.
 func ResizeKeepAspectRatio(srcPath, dstPath string, maxWidth, maxHeight int) (err error) {
-	srcImg, srcExt, err := processSrc(srcPath)
+	srcImg, _, err := DecodeFromPath(srcPath)
 	if err != nil {
 		return
 	}
@@ -90,12 +49,12 @@ func ResizeKeepAspectRatio(srcPath, dstPath string, maxWidth, maxHeight int) (er
 		return
 	}
 
-	return processDst(dstPath, dstImg, srcExt)
+	return EncodeToPath(dstPath, dstImg)
 }
 
 // ResizeByWidth 按宽度等比例缩放图片.
 func ResizeByWidth(srcPath, dstPath string, width int) (err error) {
-	srcImg, srcExt, err := processSrc(srcPath)
+	srcImg, _, err := DecodeFromPath(srcPath)
 	if err != nil {
 		return
 	}
@@ -105,12 +64,12 @@ func ResizeByWidth(srcPath, dstPath string, width int) (err error) {
 		return
 	}
 
-	return processDst(dstPath, dstImg, srcExt)
+	return EncodeToPath(dstPath, dstImg)
 }
 
 // ResizeByHeight 按高度等比例缩放图片.
 func ResizeByHeight(srcPath, dstPath string, height int) (err error) {
-	srcImg, srcExt, err := processSrc(srcPath)
+	srcImg, _, err := DecodeFromPath(srcPath)
 	if err != nil {
 		return
 	}
@@ -120,5 +79,5 @@ func ResizeByHeight(srcPath, dstPath string, height int) (err error) {
 		return
 	}
 
-	return processDst(dstPath, dstImg, srcExt)
+	return EncodeToPath(dstPath, dstImg)
 }
