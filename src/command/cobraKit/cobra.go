247d39083bec4f44ebc4e01ee929ad614240f050
Richelieu <richelieu042@gmail.com>
2026-01-20 14:42:49 +0800

    update

--- src/command/cobraKit/cobra.go
+++ src/command/cobraKit/cobra.go
@@ -4,6 +4,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// NewSimpleCommand
+/*
+@param use		命令的名称
+@param short	命令的 "简短描述" ，帮助用户快速理解其功能
+@param long		命令的 "详细描述" ，进一步解释工具的用途
+@param run		定义了当执行该命令时的行为
+*/
 func NewSimpleCommand(use, short, long string, run func(cmd *cobra.Command, args []string)) *cobra.Command {
 	return &cobra.Command{
 		Use:   use,
