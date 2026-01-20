8d1c49d0f9eba45a57cb03df07b68be88164107f
Richelieu <richelieu042@gmail.com>
2026-01-20 14:54:28 +0800

    update

--- src/command/cobraKit/cobra.go
+++ src/command/cobraKit/cobra.go
@@ -8,11 +8,10 @@ import (
 /*
 PS:
 (1) 自动支持 "-h" 或 "--help" 标识.
-(2)
 
 @param use		命令的名称
 @param short	命令的 "简短描述" ，帮助用户快速理解其功能
-@param long		命令的 "详细描述" ，进一步解释工具的用途
+@param long		命令的 "详细描述" ，进一步解释工具的用途（如果为空，会使用 short）
 @param run		定义了当执行该命令时的行为
 */
 func NewSimpleCommand(use, short, long string, run func(cmd *cobra.Command, args []string)) *cobra.Command {
