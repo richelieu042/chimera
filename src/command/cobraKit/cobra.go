6547430b139a89e7acd9cd13c6132b783f119b36
Richelieu <richelieu042@gmail.com>
2026-01-21 15:04:18 +0800

    update

--- src/command/cobraKit/cobra.go
+++ src/command/cobraKit/cobra.go
@@ -11,7 +11,9 @@ PS:
 
 @param use		命令的名称
 @param short	命令的 "简短描述" ，帮助用户快速理解其功能
-@param long		命令的 "详细描述" ，进一步解释工具的用途（如果为空，会使用 short）
+@param long		命令的 "详细描述" ，进一步解释工具的用途
+				（1）如果为空，会使用 short
+				（2）可以是多行的文本
 @param run		定义了当执行该命令时的行为
 */
 func NewSimpleCommand(use, short, long string, run func(cmd *cobra.Command, args []string)) *cobra.Command {
