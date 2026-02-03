691f1d0b80ded5a1da323c26c8aa49d707c6d2f5
Richelieu <richelieu042@gmail.com>
2026-02-03 11:46:29 +0800

    update

--- src/android/adbKit/instance_operation.go
+++ src/android/adbKit/instance_operation.go
@@ -7,6 +7,7 @@ import (
 	"github.com/richelieu-yang/chimera/v3/src/command/cmdKit"
 	"github.com/richelieu-yang/chimera/v3/src/core/errorKit"
 	"github.com/richelieu-yang/chimera/v3/src/core/intKit"
+	"github.com/richelieu-yang/chimera/v3/src/core/strKit"
 	"github.com/richelieu-yang/chimera/v3/src/file/fileKit"
 )
 
@@ -44,8 +45,14 @@ func (ins *Instance) Screenshot(targetPath string) error {
 	命令：adb -s 127.0.0.1:5555 shell input tap <x> <y>
 */
 func (ins *Instance) Tap(x, y int) error {
-	_, err := cmdKit.RunCombinedly(context.TODO(), "adb", "-s", ins.address, "shell", "input", "tap", intKit.IntToString(x), intKit.IntToString(y))
-	return err
+	resp, err := cmdKit.RunCombinedlyToString(context.TODO(), true, "adb", "-s", ins.address, "shell", "input", "tap", intKit.IntToString(x), intKit.IntToString(y))
+	if err != nil {
+		return err
+	}
+	if strKit.IsNotEmpty(resp) {
+		return errorKit.Newf("fail to tap, response: %s", resp)
+	}
+	return nil
 }
 
 // LongPress 长按.
@@ -59,10 +66,16 @@ func (ins *Instance) LongPress(x, y int, duration int) error {
 		duration = 300 // 默认300ms
 	}
 
-	_, err := cmdKit.RunCombinedly(context.TODO(), "adb", "-s", ins.address, "shell", "input", "swipe", intKit.IntToString(x), intKit.IntToString(y), intKit.IntToString(x), intKit.IntToString(y),
+	resp, err := cmdKit.RunCombinedlyToString(context.TODO(), true, "adb", "-s", ins.address, "shell", "input", "swipe", intKit.IntToString(x), intKit.IntToString(y), intKit.IntToString(x), intKit.IntToString(y),
 		intKit.IntToString(duration),
 	)
-	return err
+	if err != nil {
+		return err
+	}
+	if strKit.IsNotEmpty(resp) {
+		return errorKit.Newf("fail to tap, response: %s", resp)
+	}
+	return nil
 }
 
 // Swipe 滑动.
@@ -74,23 +87,29 @@ func (ins *Instance) LongPress(x, y int, duration int) error {
 	@param duration: 持续时间（单位：ms），默认300ms
 
 	e.g. 	向上滑动（上滑刷新/滚动）
-		adb shell input swipe 500 1500 500 500 300
+		adb -s 127.0.0.1:5555 shell input swipe 500 1500 500 500 300
 	e.g.1 	向下滑动（下拉通知栏）
-		adb shell input swipe 500 100 500 1000 300
+		adb -s 127.0.0.1:5555 shell input swipe 500 100 500 1000 300
 	e.g.2 	向左滑动
-		adb shell input swipe 900 500 100 500 300
+		adb -s 127.0.0.1:5555 shell input swipe 900 500 100 500 300
 	e.g.3 	向右滑动
-		adb shell input swipe 100 500 900 500 300
+		adb -s 127.0.0.1:5555 shell input swipe 100 500 900 500 300
 */
 func (ins *Instance) Swipe(x1, y1, x2, y2 int, duration int) error {
 	if duration <= 0 {
 		duration = 300 // 默认300ms
 	}
 
-	_, err := cmdKit.RunCombinedly(context.TODO(), "adb", "-s", ins.address, "shell", "input", "swipe",
+	resp, err := cmdKit.RunCombinedlyToString(context.TODO(), true, "adb", "-s", ins.address, "shell", "input", "swipe",
 		intKit.IntToString(x1), intKit.IntToString(y1),
 		intKit.IntToString(x2), intKit.IntToString(y2),
 		intKit.IntToString(duration),
 	)
-	return err
+	if err != nil {
+		return err
+	}
+	if strKit.IsNotEmpty(resp) {
+		return errorKit.Newf("fail to tap, response: %s", resp)
+	}
+	return nil
 }
