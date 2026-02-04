dc2f8524437e7bf227bf5a4d57ed4defaa5d0bfe
Richelieu <richelieu042@gmail.com>
2026-02-04 16:58:31 +0800

    update

--- src/config/yaml/yamlKit/marshal.go
+++ src/config/yaml/yamlKit/marshal.go
@@ -9,12 +9,16 @@ import (
 
 // Marshal
 /*
+Deprecated: Use goYamlKit.Marshal as replacement.
+
 PS: 需要搭配 yaml tag 一起使用，不识别 json tag.
 */
 var Marshal func(in interface{}) (out []byte, err error) = yaml.Marshal
 
 // MarshalToString
 /*
+Deprecated: Use goYamlKit.Marshal as replacement.
+
 PS: 需要搭配 yaml tag 一起使用，不识别 json tag.
 
 @param in 建议为结构体实例指针 || map实例 || slice实例
@@ -26,6 +30,8 @@ func MarshalToString(in interface{}) (string, error) {
 
 // MarshalToFile
 /*
+Deprecated: Use goYamlKit.Marshal as replacement.
+
 PS:
 (1) 需要搭配 yaml tag 一起使用，不识别 json tag.
 (2) 对 传参filePath 的验证和断言在 fileKit.WriteToFile 里面.
--- src/config/yaml/yamlKit/unmarshal.go
+++ src/config/yaml/yamlKit/unmarshal.go
@@ -7,18 +7,26 @@ import (
 
 // Unmarshal
 /*
+Deprecated: Use goYamlKit.Unmarshal as replacement.
+
 PS: 需要搭配 yaml tag 一起使用，不识别 json tag.
 */
 var Unmarshal func(in []byte, out interface{}) (err error) = yaml.Unmarshal
 
 // UnmarshalFromString
 /*
+Deprecated: Use goYamlKit.Unmarshal as replacement.
+
 PS: 需要搭配 yaml tag 一起使用，不识别 json tag.
 */
 func UnmarshalFromString(in string, out interface{}) error {
 	return Unmarshal([]byte(in), out)
 }
 
+// UnmarshalFromFile
+/*
+Deprecated: Use goYamlKit.Unmarshal as replacement.
+*/
 func UnmarshalFromFile(path string, out interface{}) error {
 	data, err := fileKit.ReadFile(path)
 	if err != nil {
