6143bf64427a7f63f0a0dea23e832c535002a778
Richelieu <richelieu042@gmail.com>
2026-02-04 17:04:47 +0800

    update

--- src/config/yaml/goYamlKit/marshal.go
+++ src/config/yaml/goYamlKit/marshal.go
@@ -1,9 +1,16 @@
 package goYamlKit
 
 import (
+	"context"
+
 	"github.com/goccy/go-yaml"
 )
 
+// Marshal 序列化（结构体实例 => yaml文本）
 func Marshal(v interface{}) ([]byte, error) {
 	return yaml.Marshal(v)
+}
+
+func MarshalWithContextAndOptions(ctx context.Context, v interface{}, opts ...yaml.EncodeOption) ([]byte, error) {
+	return yaml.MarshalContext(ctx, v, opts...)
 }
--- src/config/yaml/goYamlKit/unmarshal.go
+++ src/config/yaml/goYamlKit/unmarshal.go
@@ -1,7 +1,16 @@
 package goYamlKit
 
-import "github.com/goccy/go-yaml"
+import (
+	"context"
 
+	"github.com/goccy/go-yaml"
+)
+
+// Unmarshal 反序列化（yaml文本 => 结构体实例）
 func Unmarshal(data []byte, v interface{}) error {
 	return yaml.Unmarshal(data, v)
+}
+
+func UnmarshalWithContextAndOptions(ctx context.Context, data []byte, v interface{}, opts ...yaml.DecodeOption) error {
+	return yaml.UnmarshalContext(ctx, data, v, opts...)
 }
