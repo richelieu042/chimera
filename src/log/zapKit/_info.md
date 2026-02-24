4b7aac219187245f0369eecf0e8aed8637ea70cc
Richelieu <richelieu042@gmail.com>
2026-02-24 17:18:11 +0800

    update

--- src/log/zapKit/_info.md
+++ src/log/zapKit/_info.md
@@ -4,7 +4,7 @@
 
 ## 参考
 
-- notes/Golang/log（日志）/zap.wps
+- notes/Golang/log（日志）/zap.docx
 - [github 21.2k](https://github.com/uber-go/zap)
 - [Go 语言之 zap 日志库简单使用](https://zhuanlan.zhihu.com/p/637747131)
 
--- test/test.go
+++ test/test.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"time"
 
-	"github.com/richelieu042/chimera/v3/src/core/strKit"
+	"github.com/richelieu042/chimera/v3/src/time/timeKit"
 )
 
 func main() {
+	now := time.Now()
+	fmt.Println(timeKit.Format(now, "2006-01-02T15.04.05.000"))
+	fmt.Println(timeKit.Format(now, "2006-01-02T15.04"))
+	fmt.Println(timeKit.Format(now, "05.000"))
 
-	fmt.Println(strKit.Replace("abcdcba", "a", "0", 1))  // "0bcdcba"
-	fmt.Println(strKit.Replace("abcdcba", "a", "0", 2))  // "0bcdcb0"
-	fmt.Println(strKit.Replace("abcdcba", "a", "0", -1)) // "0bcdcb0"
+	//fmt.Println(strKit.Replace("abcdcba", "a", "0", 1))  // "0bcdcba"
+	//fmt.Println(strKit.Replace("abcdcba", "a", "0", 2))  // "0bcdcb0"
+	//fmt.Println(strKit.Replace("abcdcba", "a", "0", -1)) // "0bcdcb0"
 
 }
