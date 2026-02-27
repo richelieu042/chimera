ed4439ff9f88246fef9ed314f2b40993ec34fe56
Richelieu <richelieu042@gmail.com>
2026-02-27 17:08:14 +0800

    update

--- test/test.go
+++ test/test.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/richelieu042/chimera/v3/src/log/zapKit"
-)
-
 func main() {
-	w := os.Stdout
-	ws := zapKit.NewLockedWriteSyncer(w)
-	fmt.Println(ws == os.Stdout)
 
-	{
-		w := os.Stderr
-		ws := zapKit.NewLockedWriteSyncer(w)
-		fmt.Println(ws == os.Stderr)
-	}
 }
