ea430ebc1d7c9bcf822942bd8940fe3803f0aa10
Richelieu <richelieu042@gmail.com>
2026-03-02 15:55:27 +0800

    update

--- src/core/error/errKit/error.go
+++ src/core/error/errKit/error.go
@@ -32,8 +32,9 @@ var (
 	/*
 		用于检查错误链中是否存在某个特定类型的错误，并将其提取出来。
 	*/
-	As  func(err error, target interface{}) bool = errors.As
-	As1 func(err error, target any) bool         = goerrors.As
+	As func(err error, target interface{}) bool = errors.As
+	// As1 Deprecated: 推荐使用 As，原因：分布式系统 / RPC 场景：cockroachdb 有跨节点错误类型恢复能力，是其核心优势
+	As1 func(err error, target any) bool = goerrors.As
 
 	Unwrap     func(err error) error = errors.Unwrap
 	UnwrapOnce func(err error) error = errors.UnwrapOnce
