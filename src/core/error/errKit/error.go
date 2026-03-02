d0602135ad6e46be1c58a400b9cae6303cf9add0
Richelieu <richelieu042@gmail.com>
2026-03-02 15:25:47 +0800

    update

--- src/core/error/errKit/error.go
+++ src/core/error/errKit/error.go
@@ -8,19 +8,28 @@ import (
 )
 
 var (
+	// Simple 创建一个简单的错误，不包含堆栈信息.
+	Simple func(text string) error = goerrors.New
+
 	New           func(msg string) error                                    = errors.New
 	Newf          func(format string, args ...interface{}) error            = errors.Newf
 	NewWithDepth  func(depth int, msg string) error                         = errors.NewWithDepth
 	NewWithDepthf func(depth int, format string, args ...interface{}) error = errors.NewWithDepthf
 
-	As  func(err error, target interface{}) bool = errors.As
-	As1 func(err error, target any) bool         = goerrors.As
-	Is  func(err, target error) bool             = goerrors.Is
-
 	Wrap           func(err error, msg string) error                                    = errors.Wrap
 	Wrapf          func(err error, format string, args ...interface{}) error            = errors.Wrapf
 	WrapWithDepth  func(depth int, err error, msg string) error                         = errors.WrapWithDepth
 	WrapWithDepthf func(depth int, err error, format string, args ...interface{}) error = errors.WrapWithDepthf
+
+	As  func(err error, target interface{}) bool = errors.As
+	As1 func(err error, target any) bool         = goerrors.As
+
+	// Is 判断错误是否匹配某个目标值.
+	/*
+		（1）用于检查错误链中是否包含某个特定的错误值（用 == 语义比较）。
+		（2）即使被包装，也能匹配到
+	*/
+	Is func(err, target error) bool = goerrors.Is
 
 	Unwrap     func(err error) error = errors.Unwrap
 	UnwrapOnce func(err error) error = errors.UnwrapOnce
--- src/core/error/errKit/error_test.go
+++ src/core/error/errKit/error_test.go
@@ -1,14 +1,13 @@
 package errKit
 
 import (
+	goerrors "errors"
 	"fmt"
 	"testing"
 
 	"github.com/redis/go-redis/v9"
 	"github.com/richelieu042/chimera/v3/src/log/console"
 	"go.uber.org/zap"
-
-	goerrors "errors"
 )
 
 func TestNew(t *testing.T) {
@@ -31,8 +30,34 @@ func TestWrap(t *testing.T) {
 	console.Errorf("hello world: %+v", err)
 }
 
+func TestIs(t *testing.T) {
+	var ErrNotFound = goerrors.New("not found")
+	var findUser = func(id int) error {
+		return fmt.Errorf("findUser: %w", ErrNotFound) // 用 %w 包装错误
+	}
+
+	err := findUser(42)
+	if Is(err, ErrNotFound) {
+		fmt.Println("用户不存在") // ✅ 即使被包装，也能匹配到
+	}
+}
+
+func TestIs_1(t *testing.T) {
+	var ErrNotFound = goerrors.New("not found")
+	var findUser = func(id int) error {
+		return Wrap(ErrNotFound, "findUser") // 用 Wrap 包装
+	}
+
+	err := findUser(42)
+	if Is(err, ErrNotFound) {
+		fmt.Println("用户不存在") // ✅ 即使被包装，也能匹配到
+	}
+}
+
 func TestAs(t *testing.T) {
-	err := Wrap(redis.Nil, "ccc")
-	fmt.Println(goerrors.As(err, redis.Nil))
-	//fmt.Println(As(redis.Nil, err))
+
+}
+
+func TestAs1(t *testing.T) {
+
 }
