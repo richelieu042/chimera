6ae85d738f0d12984207e5676dfe35a75570c598
Richelieu <richelieu042@gmail.com>
2026-03-02 16:08:52 +0800

    update

--- src/core/error/errKit/error.go
+++ src/core/error/errKit/error.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	// Simple 创建一个简单的错误，不包含堆栈信息.
+	// Simple 创建一个简单的错误（不包含堆栈信息）.
 	Simple func(text string) error = goerrors.New
 
 	New           func(msg string) error                                    = errors.New
@@ -21,6 +21,15 @@ var (
 	WrapWithDepth  func(depth int, err error, msg string) error                         = errors.WrapWithDepth
 	WrapWithDepthf func(depth int, err error, format string, args ...interface{}) error = errors.WrapWithDepthf
 
+	Unwrap     func(err error) error = errors.Unwrap
+	UnwrapOnce func(err error) error = errors.UnwrapOnce
+	UnwrapAll  func(err error) error = errors.UnwrapAll
+
+	EncodeError func(ctx context.Context, err error) errors.EncodedError = errors.EncodeError
+	DecodeError func(ctx context.Context, enc errors.EncodedError) error = errors.DecodeError
+)
+
+var (
 	// Is 判断错误是否匹配某个目标值.
 	/*
 		（1）用于检查错误链中是否包含某个特定的错误值（用 == 语义比较）
@@ -33,16 +42,10 @@ var (
 		用于检查错误链中是否存在某个特定类型的错误，并将其提取出来。
 	*/
 	As func(err error, target interface{}) bool = errors.As
+
 	// As1
 	/*
 		Deprecated: 推荐使用 As，原因：（分布式系统 / RPC 场景）cockroachdb 有跨节点错误类型恢复能力，是其核心优势
 	*/
 	As1 func(err error, target any) bool = goerrors.As
-
-	Unwrap     func(err error) error = errors.Unwrap
-	UnwrapOnce func(err error) error = errors.UnwrapOnce
-	UnwrapAll  func(err error) error = errors.UnwrapAll
-
-	EncodeError func(ctx context.Context, err error) errors.EncodedError = errors.EncodeError
-	DecodeError func(ctx context.Context, enc errors.EncodedError) error = errors.DecodeError
 )
