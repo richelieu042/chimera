2404129bfd90db5c87fafe49468431d34b079f2e
Richelieu <richelieu042@gmail.com>
2026-03-04 14:29:57 +0800

    update

--- src/log/zapKit/core.go
+++ src/log/zapKit/core.go
@@ -33,7 +33,7 @@ import (
 */
 func NewCore(enc zapcore.Encoder, writeSyncer zapcore.WriteSyncer, levelEnabler zapcore.LevelEnabler, initialFields ...zapcore.Field) zapcore.Core {
 	if writeSyncer == nil {
-		// 杜宇 os.Stdout，不加 Lock 也不会出现日志行内容错乱的问题
+		// 对于 os.Stdout，不加 Lock 也不会出现日志行内容错乱的问题
 		writeSyncer = os.Stdout
 	}
 
