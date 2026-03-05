34b7ffa62ca5b89151eb752847d053d1676bef89
Richelieu <richelieu042@gmail.com>
2026-03-05 14:04:45 +0800

    update

--- test/test.go
+++ test/test.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/richelieu042/chimera/v3/src/image/imageKit"
-	"github.com/richelieu042/chimera/v3/src/ocr/gosseractKit"
-)
-
 func main() {
-	fmt.Println(imageKit.ToGrayscaleWithPath("clipped.png", "1.png"))
 
-	fmt.Println(gosseractKit.GertText("clipped.png", "chi_sim", "eng"))
-	fmt.Println(gosseractKit.GertText("1.png", "chi_sim", "eng"))
 }
