4091f768756ca2ea1961d189d9967d6426b05607
Richelieu <richelieu042@gmail.com>
2026-03-16 14:08:07 +0800

    update

---
+++ src/gocvKit/structs.go
@@ -0,0 +1,18 @@
+package gocvKit
+
+import "image"
+
+type (
+	Point struct {
+		image.Point
+	}
+)
+
+func NewPoint(x, y int) *Point {
+	return &Point{
+		Point: image.Point{
+			X: x,
+			Y: y,
+		},
+	}
+}
