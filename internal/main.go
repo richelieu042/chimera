a7c7b5fc948415d12ac0b42918c0141146d7746d
Richelieu <richelieu042@gmail.com>
2026-03-16 15:18:56 +0800

    update

--- internal/main.go
+++ internal/main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"github.com/richelieu042/chimera/v3/src/log/zapKit"
-	"go.uber.org/zap"
-)
-
 func main() {
-	logger := zapKit.NewSimpleConsoleLogger()
 
-	logger.Info("hello world", zap.Bool("flag", true), zap.Float64("f", 3.1415926))
-	logger.Sugar().Info("hello world", zap.Bool("flag", true), zap.Float64("f", 3.1415926))
 }
--- src/gocvKit/_info.md
+++ src/gocvKit/_info.md
@@ -9,3 +9,8 @@ var img gocv.Mat 只是声明了零值 Mat（相当于 gocv.Mat{}），没有调
 - gocv.IMRead(...)
 - gocv.CvtColor 的输出 Mat（你代码里 img = gocv.NewMat() 那种）
 - 其他会返回/填充新 Mat 的函数
+
+### case: 零值（gocv.Mat{}）调用 Close()
+
+一切正常，会返回 nil（并不会发生 panic 等）。
+但不推荐这么干。
