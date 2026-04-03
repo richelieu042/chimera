15b91737e163117a60d28e27d21833b37c803b27
Richelieu <richelieu042@gmail.com>
2026-04-03 14:06:28 +0800

    fmt

--- src/log/zapKit/file_logger.go
+++ src/log/zapKit/file_logger.go
@@ -1,21 +1,39 @@
 package zapKit
 
 import (
-	"github.com/richelieu042/chimera/v3/src/file/fileKit"
+	"os"
+
+	"github.com/gogf/gf/v2/os/gfile"
 	"go.uber.org/zap/zapcore"
 )
 
 // NewFileLogger
 /*
-PS: 仅输出到文件.
+PS:
+（1）仅输出到文件；
+（2）不要调用 fileKit，以免 import cycle.
 
 @param filePath 日志文件路径
 */
 func NewFileLogger(filePath string, prefix string, level zapcore.Level, loggerOptions ...LoggerOption) (*WrappedLogger, error) {
-	f, err := fileKit.CreateInAppendMode(filePath)
+	/* 参考了：fileKit.CreateInAppendMode */
+	// 创建父目录（如果不存在的话）
+	parentDir := gfile.Dir(filePath)
+	if !gfile.Exists(parentDir) {
+		if err := gfile.Mkdir(parentDir); err != nil {
+			return nil, err
+		}
+	}
+	// 打开文件（此处不要关闭文件，已关闭就写不了日志了）
+	f, err := gfile.OpenFile(filePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644) // 如果文件有内容，会append
 	if err != nil {
 		return nil, err
 	}
+
+	//f, err := fileKit.CreateInAppendMode(filePath)
+	//if err != nil {
+	//	return nil, err
+	//}
 
 	enc := NewEncoder(WithEncoderMessagePrefix(prefix))
 	core := NewCore(enc, zapcore.Lock(f), level)
