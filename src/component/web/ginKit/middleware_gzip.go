dca447af7a9ab3e33b4aed72b5b06d5b5da51180
Richelieu <richelieu042@gmail.com>
2026-04-13 14:20:05 +0800

    ### fix: gin zip中间件，涉及请求转发（forward）时，走 Content-Length 快速路径时，应该在调用底层 writer 之前就移除头部，而不是依赖 defer

--- src/component/web/ginKit/middleware_gzip.go
+++ src/component/web/ginKit/middleware_gzip.go
@@ -8,7 +8,7 @@ import (
 
 // NewGzipMiddleware
 /*
-Richelieu: 官方版（github.com/gin-contrib/gzip v1.2.6）有问题，因此使用了自己的修改版（github.com/richelieu042/gin-gzip-middleware）.
+Richelieu: 官方版（github.com/gin-contrib/gzip v1.2.6）在请求转发forward时有问题，因此使用了自己的修改版（github.com/richelieu042/gin-gzip-middleware）.
 
 PS:
 (1) 涉及多个服务（请求转发）的场景下，(a) 最外层的务使用gzip压缩;
