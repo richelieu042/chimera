6e325220ebf4b97e7328a6f3e41d78e5c7f3a71a
Richelieu <richelieu042@gmail.com>
2026-04-13 15:36:59 +0800

    update

--- src/component/web/ginKit/middleware_gzip.go
+++ src/component/web/ginKit/middleware_gzip.go
@@ -17,7 +17,7 @@ PS:
 
 @param level 压缩级别，此参数直接映射自 Go 标准库 compress/gzip，共有以下值：
 				gzip.HuffmanOnly			-2		仅使用 Huffman 编码，不做 LZ77 匹配，速度极快但压缩率低
-				gzip.DefaultCompression		-1		默认压缩级别（推荐，平衡速度与压缩率）
+				gzip.DefaultCompression		-1		默认压缩级别（推荐，平衡速度与压缩率）（对应的实际级别是 6）
 				gzip.NoCompression			0		不压缩（仅封装 gzip 格式，无实际压缩）
 				gzip.BestSpeed				1		最快速度，压缩率最低
 				2~8							2~8		中间级别，数字越大压缩率越高、速度越慢
