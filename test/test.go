b30c858dcc0216fa1867331b0c596f83c12205ec
Richelieu <richelieu042@gmail.com>
2026-04-21 13:25:17 +0800

    bump dependencies

--- test/test.go
+++ test/test.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"regexp"
-	"strconv"
-)
-
 func main() {
-	fmt.Println(getDays("阿是擦上次   1.5天期望的   2天"))
-}
 
-// getDays 提取字符串中"天"前面的数字（严格匹配）
-func getDays(s string) (float64, error) {
-	re := regexp.MustCompile(`(\d+\.?\d*)天`)
-	match := re.FindStringSubmatch(s)
-
-	if len(match) > 1 {
-		return strconv.ParseFloat(match[1], 64)
-	}
-	return 0, fmt.Errorf("invalid string: %s", s)
 }
