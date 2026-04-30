06bc54edb558a68d6e71084a8ea7292ce04de979
Richelieu <richelieu042@gmail.com>
2026-04-30 13:59:39 +0800

    update

--- src/component/web/ginKit/no_route.go
+++ src/component/web/ginKit/no_route.go
@@ -9,22 +9,15 @@ import (
 
 // DefaultNoRouteHtml 使用自带的404页面.
 func DefaultNoRouteHtml(engine *gin.Engine) error {
-	//templ := template.Must(template.Sign("").ParseFS(efs, "_html/*.html"))
 	templ, err := template.New("").ParseFS(efs, "_html/*.html")
 	if err != nil {
 		return err
 	}
 	engine.SetHTMLTemplate(templ)
 
-	//var prefix string
-	//if strKit.IsNotEmpty(serviceInfo) {
-	//	prefix = fmt.Sprintf("[%s] ", serviceInfo)
-	//}
-
 	engine.NoRoute(func(ctx *gin.Context) {
 		ctx.HTML(http.StatusNotFound, "404.min.html", gin.H{
-			//"prefix": prefix,
-			"route": ctx.Request.URL.Path,
+			//"route": ctx.Request.URL.Path,
 		})
 	})
 	return nil
