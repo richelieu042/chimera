2780de6abe630c0d7cf90b6eaf489b8caf78bb69
Richelieu <richelieu042@gmail.com>
2026-08-28 13:52:32 +0800

    json v2相关

--- src/serialize/json/jsonKit/api.go
+++ src/serialize/json/jsonKit/api.go
@@ -35,31 +35,3 @@ func GetDefaultApi() API {
 func GetStdApi() API {
 	return stdApi
 }
-
-//func testAPI() {
-//	api := defaultApi
-//	m := map[string]interface{}{
-//		"0": 3.1415926,
-//		"1": 1,
-//		"2": true,
-//		"3": `~!@#$%^&*()_+{}|:"><?
-//qwdqw强	\t\r\n无敌
-//qwdqwd
-//威尔法
-//496465~·《》？L:"{}|,./l;'[]\/*-的确
-//`,
-//	}
-//
-//	jsonData, err := api.Marshal(m)
-//	if err != nil {
-//		logrus.WithError(err).Fatalf("[%s, JSON] Fail to marshal!!!", consts.UpperProjectName)
-//	}
-//	var m1 map[string]interface{}
-//	if err := api.Unmarshal(jsonData, &m1); err != nil {
-//		logrus.WithError(err).Fatalf("[%s, JSON] Fail to unmarshal!!!", consts.UpperProjectName)
-//	}
-//	if compareKit.EqualWithTypeAndValue(m, m1) {
-//		diff := compareKit.Diff(m, m1)
-//		logrus.WithField("diff", diff).Fatalf("[%s, JSON] m and m1 are different.", consts.UpperProjectName)
-//	}
-//}
